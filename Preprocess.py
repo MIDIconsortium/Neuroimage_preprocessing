2ed2664b2e453bb749ed457d9505965c5c721c4d
MIDIconsortium <67752614+MIDIconsortium@users.noreply.github.com>
2022-01-19 01:33:40 +0000

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -20,13 +20,14 @@ import matplotlib.pyplot as plt
 from monai.config import print_config
 
 
-def preprocess(input_path, save_path, border=15, min_dim=130):
+def preprocess(input_path, save_path, border=15, min_dim=130, plot=True):
     
     resize = Resize(spatial_size=(120, 120, 120), mode='trilinear')
     crop_pad = ResizeWithPadOrCrop(spatial_size=(180,180,180))
     
     arr, _ = LoadNifti()(input_path)
     shape = arr.shape
+    dims = np.round(_['pixdim'][1:4],2)
     arr = AddChannel()(arr)
     arr_resampled =  Spacing(pixdim=(1., 1., 1.), mode='bilinear')(arr,_['affine'])[0]
 
@@ -60,5 +61,67 @@ def preprocess(input_path, save_path, border=15, min_dim=130):
         new_image = nib.Nifti1Image(resized, affine=np.eye(4))
 
         nib.save(new_image, save_path)
+        
+        if plot:
+            fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8) = plt.subplots(8,4, figsize = (15,15))
+
+            cropped = crop_pad(arr_resampled[:,a:-a1, b:-b1,:])
+            resized = resize(cropped)
+
+            pad = 180 - arr_resampled.shape[-1]
+
+            title = [str(a) + 'mm' for a in dims]
+            
+            ax1[0].imshow(np.rot90(arr.squeeze()[:,::-1,int(arr.shape[-1]*0.5/8)]), cmap='gray')
+            ax1[0].set_title('{}\n{}, {}, {}'.format(shape, title[0], title[1],title[2]))
+            ax1[1].imshow(np.rot90(arr_resampled.squeeze()[:,::-1,int(arr_resampled.shape[-1]*0.5/8)]), cmap='gray')
+            ax1[1].set_title('{}\n1mm, 1mm, 1mm'.format(arr_resampled.squeeze().shape, (1,1,1)))
+            ax1[2].imshow(np.rot90(cropped.squeeze()[:,::-1,int(arr_resampled.shape[-1]*0.5/8 + pad/2)]), cmap='gray')
+            ax1[2].set_title('{},\n1mm, 1mm, 1mm'.format(cropped.squeeze().shape, (1,1,1)))
+            ax1[3].imshow(np.rot90(resized.squeeze()[:,::-1,int(120/180*(arr_resampled.shape[-1]*0.5/8 + pad/2))]), cmap='gray')
+            ax1[3].set_title('(120,120,120)\n N/A')
+
+
+            ax2[0].imshow(np.rot90(arr.squeeze()[:,::-1,int(arr.shape[-1]/8)]), cmap='gray')
+            ax2[1].imshow(np.rot90(arr_resampled.squeeze()[:,::-1,int(arr_resampled.shape[-1]/8)]), cmap='gray')
+            ax2[2].imshow(np.rot90(cropped.squeeze()[:,::-1,int(arr_resampled.shape[-1]/8 + pad/2)]), cmap='gray')
+            ax2[3].imshow(np.rot90(resized.squeeze()[:,::-1,int(120/180*(arr_resampled.shape[-1]/8 + pad/2))]), cmap='gray')
+
+            ax3[0].imshow(np.rot90(arr.squeeze()[:,::-1,int(arr.shape[-1]/4)]), cmap='gray')
+            ax3[1].imshow(np.rot90(arr_resampled.squeeze()[:,::-1,int(arr_resampled.shape[-1]/4)]), cmap='gray')
+            ax3[2].imshow(np.rot90(cropped.squeeze()[:,::-1,int(arr_resampled.shape[-1]/4 + pad/2)]), cmap='gray')
+            ax3[3].imshow(np.rot90(resized.squeeze()[:,::-1,int(120/180*(arr_resampled.shape[-1]/4 + pad/2))]), cmap='gray')
+
+            ax4[0].imshow(np.rot90(arr.squeeze()[:,::-1,int(arr.shape[-1]/2)]), cmap='gray')
+            ax4[1].imshow(np.rot90(arr_resampled.squeeze()[:,::-1,int(arr_resampled.shape[-1]/2)]), cmap='gray')
+            ax4[2].imshow(np.rot90(cropped.squeeze()[:,::-1,int(arr_resampled.shape[-1]/2 + pad/2)]), cmap='gray')
+            ax4[3].imshow(np.rot90(resized.squeeze()[:,::-1,int(120/180*(arr_resampled.shape[-1]/2 + pad/2))]), cmap='gray')
+
+            ax5[0].imshow(np.rot90(arr.squeeze()[:,::-1,int(arr.shape[-1]*3/4)]), cmap='gray')
+            ax5[1].imshow(np.rot90(arr_resampled.squeeze()[:,::-1,int(arr_resampled.shape[-1]*3/4)]), cmap='gray')
+            ax5[2].imshow(np.rot90(cropped.squeeze()[:,::-1,int(arr_resampled.shape[-1]*3/4 + pad/2)]), cmap='gray')
+            ax5[3].imshow(np.rot90(resized.squeeze()[:,::-1,int(120/180*(arr_resampled.shape[-1]*3/4 + pad/2))]), cmap='gray')
+
+            ax6[0].imshow(np.rot90(arr.squeeze()[:,::-1,int(arr.shape[-1]*7/8)]), cmap='gray')
+            ax6[1].imshow(np.rot90(arr_resampled.squeeze()[:,::-1,int(arr_resampled.shape[-1]*7/8)]), cmap='gray')
+            ax6[2].imshow(np.rot90(cropped.squeeze()[:,::-1,int(arr_resampled.shape[-1]*7/8 + pad/2)]), cmap='gray')
+            ax6[3].imshow(np.rot90(resized.squeeze()[:,::-1,int(120/180*(arr_resampled.shape[-1]*7/8 + pad/2))]), cmap='gray')
+
+            ax7[0].imshow(np.rot90(arr.squeeze()[:,::-1,int(arr.shape[-1]*7.5/8)]), cmap='gray')
+            ax7[1].imshow(np.rot90(arr_resampled.squeeze()[:,::-1,int(arr_resampled.shape[-1]*7.5/8)]), cmap='gray')
+            ax7[2].imshow(np.rot90(cropped.squeeze()[:,::-1,int(arr_resampled.shape[-1]*7.5/8 + pad/2)]), cmap='gray')
+            ax7[3].imshow(np.rot90(resized.squeeze()[:,::-1,int(120/180*(arr_resampled.shape[-1]*7.5/8 + pad/2))]), cmap='gray')
+
+            ax8[0].imshow(np.rot90(arr.squeeze()[:,::-1,int(arr.shape[-1] - 1)]), cmap='gray')
+            ax8[1].imshow(np.rot90(arr_resampled.squeeze()[:,::-1,int(arr_resampled.shape[-1] - 1)]), cmap='gray')
+            ax8[2].imshow(np.rot90(cropped.squeeze()[:,::-1,int(arr_resampled.shape[-1] - 1 + pad/2)]), cmap='gray')
+            ax8[3].imshow(np.rot90(resized.squeeze()[:,::-1,int(120/180*(arr_resampled.shape[-1] - 1 + pad/2))]), cmap='gray')
+
+            for Ax in (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8):
+                for ax in Ax:
+                    ax.set_xticks([])
+                    ax.set_yticks([])
+
+            fig.savefig(save_path[:-3] + 'png')
 
 
